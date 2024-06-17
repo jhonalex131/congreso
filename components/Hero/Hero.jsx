2da205acf84ef19d751722871a6bfa48c60f7c5d
jhonalex131 <jhon37310@gmail.com>
2024-06-17 01:52:04 -0400

    Update Hero.jsx

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -13,8 +13,7 @@ const Hero = () => {
           <Fade right><span className="font-bold uppercase text-primary">ICT COMBINER</span></Fade>
           <Fade left>
             <h1 className="text-center items-center text-3xl font-bold pt-4 text-white md:text-5xl lg:w-2/3 md:leading-[3.5rem]">
-              Solusi untuk berbagai layanan pengembangan web
-            </h1>
+hola            </h1>
           </Fade>
           <Fade right>
             <p className="w-full pt-3 text-sm text-center text-white md:text-lg">
